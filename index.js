b9c6a435b2d4d00d7a4b710a77085cfb20f67bf3
Matt Dobson <mdobson4@gmail.com>
2014-09-22 09:43:28 -0400

    Updated the scout to conform to the new API.

--- index.js
+++ index.js
@@ -1,4 +1,27 @@
-var AutoScout = require('zetta-auto-scout');
-var LED = require('./led_driver');
+var util = require('util');
+var Device = require('zetta').Device;
 
-module.exports = new AutoScout('led', LED);
+var LED = module.exports = function() {
+  Device.call(this);
+};
+util.inherits(LED, Device);
+
+LED.prototype.init = function(config) {
+  config
+    .type('led')
+    .state('off')
+    .when('off', { allow: ['turn-on'] })
+    .when('on', { allow: ['turn-off'] })
+    .map('turn-on', this.turnOn)
+    .map('turn-off', this.turnOff);
+};
+
+LED.prototype.turnOn = function(cb) {
+  this.state = 'on';
+  cb();
+};
+
+LED.prototype.turnOff = function(cb) {
+  this.state = 'off';
+  cb();
+};
